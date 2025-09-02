f570c796392bbfd6d1ff165b09c9fe8fcd699a20
mihros <mmihros.qa987@gmail.com>
2025-09-02 16:21:15 +0700

    menambahka halaman landing page

--- resources/js/layouts/default-layout/components/header/Navbar.vue
+++ resources/js/layouts/default-layout/components/header/Navbar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { getAssetPath } from "@/core/helpers/assets";
-import { computed, ref } from "vue";
+import { computed, ref, onMounted, onUnmounted } from "vue";
 import KTUserMenu from "@/layouts/default-layout/components/menus/UserAccountMenu.vue";
 import KTThemeModeSwitcher from "@/layouts/default-layout/components/theme-mode/ThemeModeSwitcher.vue";
 import { ThemeModeComponent } from "@/assets/ts/layout";
@@ -23,20 +23,47 @@ const tahuns = ref<Array<Number>>([])
 for (let i = new Date().getFullYear(); i >= new Date().getFullYear() - 2; i--) {
     tahuns.value.push(i)
 }
+
+// === JAM REALTIME JAKARTA ===
+const currentTime = ref<string>("")
+let timer: number
+
+onMounted(() => {
+    const updateTime = () => {
+        const now = new Date()
+        currentTime.value = now.toLocaleTimeString("id-ID", {
+            timeZone: "Asia/Jakarta",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+        })
+    }
+    updateTime()
+    timer = window.setInterval(updateTime, 1000)
+})
+
+onUnmounted(() => {
+    clearInterval(timer)
+})
 </script>
 
 <template>
     <!--begin::Navbar-->
     <div class="app-navbar flex-shrink-0">
-        <!--begin::Theme mode-->
+        <!--begin::Jam realtime-->
         <div class="app-navbar-item me-10">
-            <select2 class="form-select-solid w-125px" :options="tahuns" v-model="tahun.tahun"></select2>
+            {{ currentTime }}
         </div>
+        <!--end::Jam realtime-->
+
+        <!--begin::Theme mode-->
+        <!-- <div class="app-navbar-item me-10">
+            <select2 class="form-select-solid w-125px" :options="tahuns" v-model="tahun.tahun"></select2>
+        </div> -->
         <!--end::Theme mode-->
 
         <!--begin::Theme mode-->
         <div class="app-navbar-item ms-1 ms-md-3">
-            <!--begin::Menu toggle-->
             <a href="#"
                 class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-30px h-30px w-md-40px h-md-40px"
                 data-kt-menu-trigger="{default:'click', lg: 'hover'}" data-kt-menu-attach="parent"
@@ -44,20 +71,17 @@ for (let i = new Date().getFullYear(); i >= new Date().getFullYear() - 2; i--) {
                 <KTIcon v-if="themeMode === 'light'" icon-name="night-day" icon-class="fs-2" />
                 <KTIcon v-else icon-name="moon" icon-class="fs-2" />
             </a>
-            <!--begin::Menu toggle-->
             <KTThemeModeSwitcher />
         </div>
         <!--end::Theme mode-->
 
         <!--begin::User menu-->
         <div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle">
-            <!--begin::Menu wrapper-->
             <div class="cursor-pointer symbol symbol-35px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}"
                 data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
                 <img :src="getAssetPath(user.photo ?? 'media/avatars/300-3.jpg')" class="rounded-3" alt="user" />
             </div>
             <KTUserMenu />
-            <!--end::Menu wrapper-->
         </div>
         <!--end::User menu-->
 
---
+++ resources/js/pages/auth/landing-page/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+<h1>hello world</h1>
+  </main>
+</template>
