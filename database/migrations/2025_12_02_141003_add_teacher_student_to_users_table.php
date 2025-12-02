7862eedb9d9d21df65e62e1d0a0ce07ced84156a
mihros <mmihros.qa987@gmail.com>
2025-12-02 21:13:31 +0700

    membenarkan halaman master/user teacher_id yang tidak terkirim

---
+++ database/migrations/2025_12_02_141003_add_teacher_student_to_users_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('teacher_id')->nullable()->after('student_id');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['teacher_id']);
+        });
+    }
+
+};
+
